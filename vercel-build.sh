ea4647e698fc6b8959c97c869e8d0f6c7d6e4537
awpertunist-op <shivam@infilect.com>
2023-07-29 20:46:29 +0530

    Updates B

---
+++ vercel-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install Bundler
+sudo gem install bundler
+
+# Change Directory to Your Project
+cd /path/to/your/project
+
+# Install Dependencies
+bundle install
+
+# Build the Jekyll Site Locally
+jekyll build
+
